16eee237234f4959c36c5dc8b5d8d12faac3f217
Karen AG <arellanogranillo@gmail.com>
2024-09-05 09:29:51 -0600

    Update Menu.java

--- Proyecto1/Menu.java
+++ Proyecto1/Menu.java
@@ -3,57 +3,72 @@ import java.util.Scanner;
 public class Menu {
 
 
-        public void desplegarMenu() {
-            Scanner scanner = new Scanner(System.in);
+    public void desplegarMenu() {
+        Scanner scanner = new Scanner(System.in);
 
 
-            int opcion;
+        int opcion;
 
-            System.out.println("*********************************************");
-            System.out.println("*********************************************");
-            System.out.println("*************  Hola usuario :)  *************");
-            System.out.println("************* Bienvenido al Menú ************");
-            System.out.println("*********************************************");
-            System.out.println("*********************************************");
-            System.out.println("1. Encriptar");
-            System.out.println("2. Desencriptar con clave");
-            System.out.println("0. Salir");
-            System.out.print("Selecciona una opción ---> ");
-            opcion = scanner.nextInt();
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
+        System.out.println("*************  Hola usuario :)  *************");
+        System.out.println("************* Bienvenido al Menú ************");
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
+        System.out.println("1. Encriptar");
+        System.out.println("2. Desencriptar con clave");
+        System.out.println("0. Salir");
+        System.out.print("Selecciona una opción ---> ");
+        opcion = scanner.nextInt();
+        scanner.nextLine();
+
+        if (opcion == 1) {
+            //opción para encriptar con ruta de texto
+
+            System.out.print("Introduce la ruta del texto a encriptar: ");
+            String rutaArchivoDeEntrada = scanner.nextLine();
+            String textoLeido = ManejoDeArchivos.lectorDeArchivo(rutaArchivoDeEntrada);
+
+            if (textoLeido == null) {
+                System.out.println("*** Lo siento. El archivo que se proporcionó está vacío. : ( ***");
+                return;
+            }
+
+
+            System.out.print("¿Cuál será el número de desplazamiento de caracteres? ");
+
+            int claveDeDesplazamiento = scanner.nextInt();
             scanner.nextLine();
 
-            if (opcion == 1) {
-       //opción para encriptar con ruta de texto
-
-                System.out.print("Introduce la ruta del texto a encriptar: ");
-                String rutaArchivoDeEntrada = scanner.nextLine();
-                String textoLeido = ManejoDeArchivos.lectorDeArchivo(rutaArchivoDeEntrada);
-                System.out.print("¿Cuál será el número de desplazamiento de caracteres? ");
-
-                int claveDeDesplazamiento = scanner.nextInt();
-                scanner.nextLine();
-
-                EncriptadorDesencriptador encriptador = new EncriptadorDesencriptador(textoLeido, claveDeDesplazamiento);
-                String textoEncriptado = encriptador.encriptar();
+            EncriptadorDesencriptador encriptador = new EncriptadorDesencriptador(textoLeido, claveDeDesplazamiento);
+            String textoEncriptado = encriptador.encriptar();
 
 
-                System.out.print("¿Cuál será la ruta del archivo donde se guardará el texto encriptado? ");
-                String rutaArchivoDeSalida = scanner.nextLine();
+            System.out.print("¿Cuál será la ruta del archivo donde se guardará el texto encriptado? ");
+            String rutaArchivoDeSalida = scanner.nextLine();
+
+
+            ManejoDeArchivos.escribirArchivo(rutaArchivoDeSalida, textoEncriptado);
+            System.out.println("El texto encriptado se ha guardado en: " + rutaArchivoDeSalida);
+
+        } else if (opcion == 2) {
+
+            //opción para desencriptar con ruta de texto
+
+
+            System.out.print("Introduce la ruta del texto a desencriptar: ");
+            String rutaArchivoDeEntrada = scanner.nextLine();
+            String textoEncriptado = ManejoDeArchivos.lectorDeArchivo(rutaArchivoDeEntrada);
+
+
+            if (textoEncriptado == null) {
+                System.out.println("*** Lo siento. El archivo que se proporcionó está vacío. : ( ***");
+                return;
 
 
 
-                ManejoDeArchivos.escribirArchivo(rutaArchivoDeSalida, textoEncriptado);
-                System.out.println("El texto encriptado se ha guardado en: " + rutaArchivoDeSalida);
 
-            } else if (opcion == 2) {
-
-                //opción para desencriptar con ruta de texto
-
-
-                System.out.print("Introduce la ruta del texto a desencriptar: ");
-                String rutaArchivoDeEntrada = scanner.nextLine();
-                String textoEncriptado = ManejoDeArchivos.lectorDeArchivo(rutaArchivoDeEntrada);
-
+            }
                 System.out.print("Introduce el número de desplazamiento de caracteres: ");
                 int claveDeDesplazamiento = scanner.nextInt();
                 scanner.nextLine();
@@ -69,7 +84,6 @@ public class Menu {
 
             } else if (opcion == 0) {
                 System.out.println("Hasta luego :)");
-
 
 
             } else {
